891991999a6b98d9343151cd620174ee41449542
fumi36 <realist0705@gmail.com>
2024-12-16 22:33:27 +0900

    Javaプログラミング入門

---
+++ src/UseVariable.java
@@ -0,0 +1,9 @@
+public class UseVariable {
+
+    public static void main(String[] args) {
+        // 1日の秒数 = 24時間 x 60分 x 60秒
+        int ichinitiNoByousuu = 24 * 60 * 60;    // * は 掛け算を意味する記号
+
+        System.out.println(ichinitiNoByousuu);
+    }
+}
